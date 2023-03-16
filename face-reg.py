40e4122d3345391788a72c113e39dd076739e71c
Vo Huu Nam Truong <namtruong0900@gmail.com>
2023-03-16 13:18:09 +0700

    Added New Dataset, Fix mark attendance

--- face-reg.py
+++ face-reg.py
@@ -33,10 +33,10 @@ def markAttendance(name):
         for line in myDataList:
             entry = line.split(',')
             nameList.append(entry[0])
-            if name not in nameList:
-                now = datetime.now()
-                dtString = now.strftime('%H:%M:%S')
-                f.writelines(f'n{name},{dtString}')
+        if name not in nameList:
+            now = datetime.now()
+            dtString = now.strftime('%H:%M:%S')
+            f.writelines(f'\n{name},{dtString}')
 
 # FOR CAPTURING SCREEN RATHER THAN WEBCAM
 # def captureScreen(bbox=(300,300,690+300,530+300)):
@@ -68,16 +68,30 @@ while True:
         # print(faceDis)
         matchIndex = np.argmin(faceDis)
 
-        if matches[matchIndex]:
+        # if matches[matchIndex]:
+        #     name = classNames[matchIndex].upper()
+        #     # print(name)
+        #     y1, x2, y2, x1 = faceLoc
+        #     y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+        #     cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        #     cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        #     cv2.putText(img, name, (x1+6, y2-6),
+        #                 cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
+        #     markAttendance(name)
+
+        if faceDis[matchIndex] < 0.50:
             name = classNames[matchIndex].upper()
-            # print(name)
-            y1, x2, y2, x1 = faceLoc
-            y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
-            cv2.putText(img, name, (x1+6, y2-6),
-                        cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
             markAttendance(name)
+        else:
+            name = 'Unknown'
+        # print(name)
+        y1, x2, y2, x1 = faceLoc
+        y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+        cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, name, (x1+6, y2-6),
+                    cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
 
     cv2.imshow('Webcam', img)
-    cv2.waitKey(1)
+
+    cv2.waitKey(16)
