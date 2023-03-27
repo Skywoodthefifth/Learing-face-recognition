1a659587886b6bfda57859f1a08a21607383eaba
Vo Huu Nam Truong <namtruong0900@gmail.com>
2023-03-27 20:45:57 +0700

    Test download img form camera ESP23

---
+++ img_ESP32.py
@@ -0,0 +1,92 @@
+import face_recognition
+import cv2
+import numpy as np
+
+import math
+from sklearn import neighbors
+import os
+import os.path
+import pickle
+
+import urllib.request
+
+
+def predict(X_img, knn_clf=None, model_path=None, distance_threshold=0.6):
+    """
+    Recognizes faces in given image using a trained KNN classifier
+
+    :param knn_clf: (optional) a knn classifier object. if not specified, model_save_path must be specified.
+    :param model_path: (optional) path to a pickled knn classifier. if not specified, model_save_path must be knn_clf.
+    :param distance_threshold: (optional) distance threshold for face classification. the larger it is, the more chance
+           of mis-classifying an unknown person as a known one.
+    :return: a list of names and face locations for the recognized faces in the image: [(name, bounding box), ...].
+        For faces of unrecognized persons, the name 'unknown' will be returned.
+    """
+
+    if knn_clf is None and model_path is None:
+        raise Exception(
+            "Must supply knn classifier either thourgh knn_clf or model_path")
+
+    # Load a trained KNN model (if one was passed in)
+    if knn_clf is None:
+        with open(model_path, 'rb') as f:
+            knn_clf = pickle.load(f)
+
+    # Load image file and find face locations
+    X_face_locations = face_recognition.face_locations(X_img)
+
+    # If no faces are found in the image, return an empty result.
+    if len(X_face_locations) == 0:
+        return []
+
+    # Find encodings for faces in the test iamge
+    faces_encodings = face_recognition.face_encodings(
+        X_img, known_face_locations=X_face_locations)
+
+    # Use the KNN model to find the best matches for the test face
+    closest_distances = knn_clf.kneighbors(faces_encodings, n_neighbors=1)
+    are_matches = [closest_distances[0][i][0] <=
+                   distance_threshold for i in range(len(X_face_locations))]
+
+    # Predict classes and remove classifications that aren't within the threshold
+    return [(pred, loc) if rec else ("unknown", loc) for pred, loc, rec in zip(knn_clf.predict(faces_encodings), X_face_locations, are_matches)]
+
+
+if __name__ == '__main__':
+    with urllib.request.urlopen("http://10.10.29.93/saved-photo") as url:
+        arr = np.asarray(bytearray(url.read()), dtype=np.uint8)
+        img = cv2.imdecode(arr, -1)  # 'Load it as it is'
+
+    print("Loaded image")
+
+    width = 640
+    height = 480
+    dim = (width, height)
+
+    # resize image
+    resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+    rgb_frame = resized[:, :, ::-1]
+
+    predictions = predict(
+        rgb_frame, model_path="trained_knn_model.clf", distance_threshold=0.4)
+
+    print(predictions)
+
+    for name, (top, right, bottom, left) in predictions:
+        print(name)
+        # Draw a box around the face
+        cv2.rectangle(resized, (left, top),
+                      (right, bottom), (0, 0, 255), 2)
+
+        # Draw a label with a name below the face
+        cv2.rectangle(resized, (left, bottom - 35),
+                      (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(resized, name, (left + 6, bottom - 6),
+                    font, 1.0, (255, 255, 255), 1)
+
+    cv2.imshow('Camera', resized)
+
+    if cv2.waitKey() & 0xFF == ord('q'):
+        quit()
